59322a08eecda62c7e6310ef3dddbfcdaa0dad47
ramardo <62728576+ramardo@users.noreply.github.com>
2023-07-17 20:46:58 -0300

    Plugins act

--- plugins/propietario(a)-cmd-list.js
+++ plugins/propietario(a)-cmd-list.js
@@ -1,10 +1,9 @@
 let handler = async (m, { conn }) => {
-conn.reply(m.chat, `
-*< LISTA DE COMANDO/TEXTOS ASIGNADOS />*
-
-${Object.entries(global.db.data.sticker).map(([key, value], index) => `*${index + 1}.-*\n*CODIGO:* ${value.locked ? `*(𝚋𝚕𝚘𝚚𝚞𝚎𝚊𝚍𝚘)* ${key}` : key}\n*COMANDO/TEXTOS* ${value.text}`).join('\n\n')}
-`.trim(), null, {mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])})
-}
-handler.command = ['listcmd', 'cmdlist']
-handler.rowner = true
-export default handler
+    conn.reply(m.chat, `ღ 𝙇𝙄𝙎𝙏𝘼 𝘿𝙀 𝘾𝙊𝙈𝘼𝙉𝘿𝙊/𝙏𝙀𝙓𝙏𝙊𝙎 𝘼𝙎𝙄𝙂𝙉𝘼𝘿𝙊𝙎
+    
+    ${Object.entries(global.db.data.sticker).map(([key, value], index) => `*${index + 1}.-*\n*ღ 𝘾𝙊𝘿𝙄𝙂𝙊:* ${value.locked ? `*(𝚋𝚕𝚘𝚚𝚞𝚎𝚊𝚍𝚘)* ${key}` : key}\n*ღ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊/𝙏𝙀𝙓𝙏𝙊𝙎* ${value.text}`).join('\n\n')}
+    `.trim(), null, {mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])})
+    }
+    handler.command = ['listcmd', 'cmdlist']
+    handler.rowner = true
+    export default handler
